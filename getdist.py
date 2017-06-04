527ac82b03877d75a71af3c803ce235fa3bf5800
hatobus <s1240056@gmail.com>
2017-06-04 20:27:10 +0900

    add python2 file

---
+++ getdist.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+import time
+import RPi.GPIO as GPIO
+
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO to use on Pi
+GPIO_TRIGECHO = 15
+
+print "Ultrasonic Measurement"
+
+# Set pins as output and input
+GPIO.setup(GPIO_TRIGECHO,GPIO.OUT)  # Initial state as output
+
+
+# Set trigger to False (Low)
+GPIO.output(GPIO_TRIGECHO, False)
+
+def measure():
+  # This function measures a distance
+  # Pulse the trigger/echo line to initiate a measurement
+    GPIO.output(GPIO_TRIGECHO, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGECHO, False)
+  #ensure start time is set in case of very quick return
+    start = time.time()
+
+  # set line to input to check for start of echo response
+    GPIO.setup(GPIO_TRIGECHO, GPIO.IN)
+    while GPIO.input(GPIO_TRIGECHO)==0:
+        start = time.time()
+
+  # Wait for end of echo response
+    while GPIO.input(GPIO_TRIGECHO)==1:
+        stop = time.time()
+  
+    GPIO.setup(GPIO_TRIGECHO, GPIO.OUT)
+    GPIO.output(GPIO_TRIGECHO, False)
+
+    elapsed = stop-start
+    distance = (elapsed * 34300)/2.0
+    time.sleep(0.1)
+    return distance
+
+try:
+
+    while True:
+
+        distance = measure()
+        print "  Distance : %.1f cm" % distance
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Stop")
+    GPIO.cleanup()
+
